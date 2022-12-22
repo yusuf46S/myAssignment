d6695046a39e6dd2a145a78669558a159cf0b1f4
yusuf46S <121245906+yusuf46S@users.noreply.github.com>
2022-12-23 03:10:55 +0400

    Update assign1.py

--- assign1.py
+++ assign1.py
@@ -24,9 +24,10 @@ if page == "Introduction":
   st.markdown('Created By: ')
   st.write('Yousuf Ali Alshanfari - 46S162727')
   st.write('Abdullah Ali Alshanfari - 42S163276')
+  
 def convert_df(df):
     return df.to_csv().encode('utf-8')
-  csv = convert_df(Steam)
+  csv = convert_df(Steam):
   st.download_button(
     label="Download dataset as CSV",
     data=csv,
